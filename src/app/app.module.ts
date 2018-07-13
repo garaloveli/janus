aa0c6bf79683b07a2f505b70765d2f8312a0edb0
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2018-07-13 18:00:31 -0500

    Updating styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HowWorksComponent } from './how-works/how-works.component';
+import { MigrateComponent } from './migrate/migrate.component';
+import { CreditsComponent } from './credits/credits.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HowWorksComponent,
+    MigrateComponent,
+    CreditsComponent,
+    ContactComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/migrate/migrate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jns-migrate',
+  templateUrl: './migrate.component.html',
+  styleUrls: ['./migrate.component.scss']
+})
+export class MigrateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
