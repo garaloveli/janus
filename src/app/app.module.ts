5abef034a67048a3698060244868ad2162496ccc
Olga Zegarra Ordóñez <garaloveli@gmail.com>
2018-07-15 13:43:02 -0500

    Adding sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { CreditsComponent } from './credits/credits.component';
+import { MigrateComponent } from './migrate/migrate.component';
+import { HowWorksComponent } from './how-works/how-works.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ContactComponent,
+    CreditsComponent,
+    MigrateComponent,
+    HowWorksComponent,
   ],
   imports: [
     BrowserModule,
